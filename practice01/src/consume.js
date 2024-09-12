817fee212b6644e86f6f8f9e6539396f4b05bea6
Dobby 이현웅 <lhw0507@midasit.com>
2024-09-12 18:40:23 +0900

    name 출력 consume

--- practice01/src/consume.js
+++ practice01/src/consume.js
@@ -1,7 +1,16 @@
 const amqp = require('amqplib');
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+    { name: 'param', alias: 'v', type: String },
+    { name: 'src', type: String, multiple: true, defaultOption: true }
+];
 
 async function receiveMessage() {
     try {
+        const commandLine = commandLineArgs(optionDefinitions);
+        const params = commandLine.src ?? [];
+
         // RabbitMQ 서버에 연결
         const connection = await amqp.connect('amqp://localhost');
 
@@ -11,29 +20,21 @@ async function receiveMessage() {
         // 큐 선언
         const queue = 'task_queue';
         await channel.assertQueue(queue, {
-            durable: true // 메시지를 영구적으로 유지
+            durable: true, // 메시지가 RabbitMQ 서버가 재시작되어도 유지되도록 durable 옵션 설정
         });
 
-        console.log(`[*] Waiting for messages in ${queue}. To exit press CTRL+C`);
-
-        // 메시지 소비 (consume)
-        channel.consume(queue, (msg) => {
-            if (msg !== null) {
-                const content = msg.content.toString();
-                console.log(`[CONSUME] Received ${content}`);
-
-                const payload = JSON.parse(content);
-                // do something with payload
-                const date = new Date(payload.date);
-                console.log(`  [CONSUME] ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} : hello ${payload.name}`)
-
-                // 메시지 처리 완료 시 ack 보내기
-                channel.ack(msg);
-            }
+        // 메시지 받기
+        channel.consume(queue, (message) => {
+            const content = JSON.parse(message.content.toString());
+            console.log(`[CONSUME] Received '${content.name}'`);
         }, {
-            noAck: false // 수신 확인 (acknowledge) 설정
+            noAck: true // 메시지 처리 확인을 자동으로 보내지 않음
         });
-    } catch (error) {
+
+        console.log('[CONSUME] Waiting for messages. To exit press CTRL+C');
+        
+    }
+    catch (error) {
         console.error('Error occurred:', error);
     }
 }
