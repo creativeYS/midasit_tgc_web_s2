87cd31240bfe3646a937ca795dda0ee64fc9c5e3
홍진 박 <phj0319@midasit.co.kr>
2024-09-13 11:49:21 +0900

    [pratice01] consume 과제 제출

--- practice01/src/consume.js
+++ practice01/src/consume.js
@@ -1,7 +1,24 @@
 const amqp = require('amqplib');
 
+const queue = 'task_queue';
+
 async function receiveMessage() {
     try {
+        const connection = await amqp.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+
+        process.once('SIGINT', async () => { 
+            await channel.close();
+            await connection.close();
+        });
+
+        await channel.assertQueue(queue, { durable: true });
+        await channel.consume(queue, (message) => {
+            const obj = JSON.parse(message.content.toString());
+            console.log(obj.name);
+        }, { noAck: true });
+
+        console.log(' [*] Waiting for messages. To exit press CTRL+C');
     } catch (error) {
         console.error('Error occurred:', error);
     }
