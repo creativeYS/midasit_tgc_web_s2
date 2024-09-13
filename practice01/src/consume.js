d83c5415684b714693d5be701a2926c35c92b935
syc1214 <syc1214@midasit.com>
2024-09-13 17:05:52 +0900

    [TGC] Consume으로 이름찍기

--- practice01/src/consume.js
+++ practice01/src/consume.js
@@ -2,6 +2,17 @@ const amqp = require('amqplib');
 
 async function receiveMessage() {
     try {
+            const connection = await amqp.connect('amqp://localhost');
+            const channel = await connection.createChannel();
+            channel.consume("task_queue", async (msg) => {
+ 
+            const msgBody = msg.content.toString();
+            const msgParse = JSON.parse(msgBody)['name'];
+            console.log(`[CONSUME] Received '${msgParse}'`);
+           
+            channel.ack(msg);
+        });
+ 
     } catch (error) {
         console.error('Error occurred:', error);
     }
