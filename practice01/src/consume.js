77311c79f767fa02ad637b588be0edaa6929f64b
pcs0807 <pcs0807@midasit.com>
2024-09-23 20:17:45 +0900

    <추가> consume 작업

--- practice01/src/consume.js
+++ practice01/src/consume.js
@@ -2,6 +2,27 @@ const amqp = require('amqplib');
 
 async function receiveMessage() {
     try {
+
+        const connection = await amqp.connect('amqp://localhost');
+
+        const channel = await connection.createChannel();
+
+        const queue = 'task_queue';
+        await channel.assertQueue(queue, {
+            durable: true
+        });
+
+        channel.consume(queue, (msg) => {
+            if (msg !== null) {
+                const content = msg.content.toString();
+                const payload = JSON.parse(content);
+                const date = new Date(payload.date);
+                channel.ack(msg);
+            }
+        }, {
+            noAck: false
+        });
+
     } catch (error) {
         console.error('Error occurred:', error);
     }
