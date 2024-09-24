0bed0f32e5a1777d39dcdf4ed0ec3b2c321bb8ed
함성훈 <shham@midasit.com>
2024-09-24 15:50:42 +0900

    과제1

--- practice01/src/consume.js
+++ practice01/src/consume.js
@@ -2,6 +2,31 @@ const amqp = require('amqplib');
 
 async function receiveMessage() {
     try {
+        // RabbitMQ 서버에 연결
+        const connection = await amqp.connect('amqp://localhost');
+
+        // 채널 생성
+        const channel = await connection.createChannel();
+
+        // 큐 선언
+        const queue = 'task_queue';
+        await channel.assertQueue(queue, {
+            durable: true, // 메시지가 RabbitMQ 서버가 재시작되어도 유지되도록 durable 옵션 설정
+        });
+
+        channel.consume(queue, (msg) => {
+            const message = msg.content.toString();
+            const name = JSON.parse(message).name;
+            console.log(name);
+
+            channel.ack(msg, true);
+        });
+
+        // 연결 닫기
+        setTimeout(() => {
+            connection.close();
+            process.exit(0);
+        }, 500);
     } catch (error) {
         console.error('Error occurred:', error);
     }
